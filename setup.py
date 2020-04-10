4c928dc625e92c243b15ed1539d949c07454762f
Matthew D. Scholefield <matthew331199@gmail.com>
2020-04-09 19:23:12 -0500

    Increment version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flask-database',
-    version='0.1.0',
+    version='0.1.1',
     description='Simple Flask database integration',
     url='https://github.com/matthewscholefield/flask-database',
     author='Matthew D. Scholefield',
